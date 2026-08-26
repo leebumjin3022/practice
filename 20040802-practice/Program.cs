4d9fbf9c0175d61112fce0ebef978b2f27aabdea
leebumjin3022 <leebumjin3022@gmail.com>
2026-08-26 12:43:57 +0900

    programming project

---
+++ 20040802-practice/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _20040802_practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
