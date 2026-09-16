50045e28f30e9bcb4b1de4fa723588b6caebfeb9
leebumjin3022 <leebumjin3022@gmail.com>
2026-09-16 13:51:20 +0900

    조건문

---
+++ practice2/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace practice2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var input = Console.ReadLine();
+            //var iValue = Int32.Parse(input);
+            //if (0 == iValue % 2)
+            //{
+            //    Console.WriteLine("짝수");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("홀수");
+            //}
+
+
+            //randomtime
+            //if (DateTime.Now.Hour < 12)
+            //{
+            //    Console.WriteLine("오전");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("오후");
+            //}
+
+            //Console.WriteLine("성적을 입력하세요 : ");
+            //var oopscore = Int32.Parse(Console.ReadLine());
+
+            //if (oopscore >= 90)
+            //{
+            //    Console.WriteLine("A");
+            //}
+            //else if (oopscore >= 80) 
+            //{
+            //    Console.WriteLine("B");
+            //}
+            //else if (oopscore >= 70)
+            //{
+            //    Console.WriteLine("C");
+            //}
+            //else if (oopscore >= 60)
+            //{
+            //    Console.WriteLine("D");
+            //}
+
+            //string input = Console.ReadLine();
+            //int ivalue = Int32.Parse(input);
+            //switch (ivalue % 2)
+            //{
+            //    case 0:
+            //        Console.WriteLine($"2의배수");
+            //        goto case 3;
+            //    case 1:
+            //        Console.WriteLine("홀수");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("기타");
+            //        break;
+            //}
+
+            //string input = Console.ReadLine();
+            //int iValue = Int32.Parse(input);
+
+            //Console.WriteLine( 0 == iValue % 2 ? "2의 배수" : "홀수");
+            //var Rank = (90 <= iValue) ? "A" :
+            //           (90 <= iValue) ? "A" \:
+            //           (90 <= iValue) ? "A":
+            //           (90 <= iValue) ? "A";
+
+            //string? input = Console.ReadLine();
+            //var age = Int32.Parse(input);
+
+            //if (18 <= age)
+            //    Console.WriteLine("성인");
+            //else
+            //    Console.WriteLine("미성년자");
+
+            int i = 0;
+            for(int i =0;i<100;++i)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
